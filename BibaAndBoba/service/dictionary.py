de96d2884f56a93c95852b98d9fdadd1ae5b2a53
Andrew Yaroshevych <ayaroshlvua@gmail.com>
2022-07-09 07:33:57 +0300

    Fixed path

--- BibaAndBoba/service/dictionary.py
+++ BibaAndBoba/service/dictionary.py
@@ -1,5 +1,5 @@
 with open("BibaAndBoba/base.lst.txt", "r") as dictionary:
-    with open("../base_ua.txt", "w") as base:
+    with open("../dictionaries/base_ua.txt", "w") as base:
         for line in dictionary:
             word = line.split()[0]
             base.write(word + "\n")
