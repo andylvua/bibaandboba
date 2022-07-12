1d7b883e1ddb722002623bfdf266553902ad2145
Andrew Yaroshevych <ayaroshlvua@gmail.com>
2022-07-13 02:09:02 +0300

    Updated documentation version

--- BibaAndBoba/docs/source/conf.py
+++ BibaAndBoba/docs/source/conf.py
@@ -21,7 +21,7 @@ copyright = '2022, Andrew Yaroshevych'
 author = 'Andrew Yaroshevych'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
+release = '1.1.0'
 
 
 # -- General configuration ---------------------------------------------------
