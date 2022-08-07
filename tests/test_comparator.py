1b09ce312bdb1cb3620d9c040074c685c40535d8
Andrew Yaroshevych <ayaroshlvua@gmail.com>
2022-08-07 13:43:14 +0300

    Added test for alternate correlation

--- tests/test_comparator.py
+++ tests/test_comparator.py
@@ -22,6 +22,13 @@ def test_get_correlation(biba_and_boba_comparator):
     assert correlation_actual == correlation_expected
 
 
+def test_alternate_correlation(biba_and_boba_comparator):
+    correlation_actual = biba_and_boba_comparator.get_correlation(use_alternate_correlation=True)
+    correlation_expected = 1.0
+
+    assert correlation_actual == correlation_expected
+
+
 def test_get_same_words(biba_and_boba_comparator):
     same_words_actual = biba_and_boba_comparator.get_same_words()
     same_words_expected = ["hi", "павло"]
